8367816579ff20c3791e6c31376c3ddf0e49d56a
Reva Nayla <revanano11@gmail.com>
2026-02-14 17:35:32 +0700

    update db

--- config/db.js
+++ config/db.js
@@ -1,20 +1,18 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME || "thinkcode",
-  process.env.DB_USER || "root",
-  process.env.DB_PASS || "",
-  {
-    host: process.env.DB_HOST || "localhost",
-    port: process.env.DB_PORT || 3306,
-    dialect: "mysql",
-    logging: false,
-    define: { timestamps: false }
-  }
-);
+const sequelize = new Sequelize(process.env.MYSQL_URL, {
+  dialect: "mysql",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 sequelize.authenticate()
-  .then(() => console.log("Database connected."))
-  .catch(err => console.log("DB ERROR:", err));
+  .then(() => console.log("✅ Database connected."))
+  .catch(err => console.log("❌ DB ERROR:", err));
 
 module.exports = sequelize;
